4c5bef51933b496215fdda74a61c8adbfb12fc14
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-19 13:06:18 +0400

    Change example status to out of maintenance

--- CS/WebSite/NewPage.aspx.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-public partial class NewPage : System.Web.UI.Page {
-    protected void Page_Load(object sender, EventArgs e) {
-        Label1.Text = "Appointment Id: " + Request.QueryString["aptId"];
-    }
-}
--- VB/WebSite/NewPage.aspx.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-
-Partial Public Class NewPage
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-        Label1.Text = "Appointment Id: " & Request.QueryString("aptId")
-    End Sub
-End Class
